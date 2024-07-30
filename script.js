874d8349294b535f34d3cf3c6c43961671d7fec9
DeikaPratama-ok <anggika.classroom@gmail.com>
2024-07-30 21:38:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/*!
+  * SweetAlert2 v11.0.19
+  * https://sweetalert2.github.io
+  * (c) 2021 SweetAlert2
+  * Released under the MIT License
+  */
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?t(exports,require("sweetalert2")):"function"==typeof define&&define.amd?define(["exports","sweetalert2"],t):t((e=e||self).SweetAlert2={},e.SweetAlert2)}(this,function(e,t){"use strict";t=t&&Object.prototype.hasOwnProperty.call(t,"default")?t.default:t,e.SweetAlert2=t,Object.defineProperty(e,"__esModule",{value:!0})});
